731cfdb524369a297afff879d9e951c9d735728e
yashikabatra15 <56561968+yashikabatra15@users.noreply.github.com>
2022-05-31 12:03:03 -0400

    pending changes; committing initial version

---
+++ hardware_tests/main.py
@@ -0,0 +1,13 @@
+"""
+FlatSat Hardware Tests: Neopixel, SDCard, Radio, IMU, Sun Sensors, Burn Wire IC
+
+Yashika Batra
+"""
+
+import hardware_tests
+
+print('Initializing PyCubed Hardware...')
+pocketqube = hardware_tests.Satellite()
+
+
+
