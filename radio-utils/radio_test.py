653f48b7bd17e8f54817c59304aaf72b4c3b4607
thetazero <lesha.seletskiy@gmail.com>
2022-08-23 17:42:09 -0400

    Update radio test

--- radio-utils/radio_test.py
+++ radio-utils/radio_test.py
@@ -4,6 +4,21 @@ from lib.naive import NaiveMessage
 import struct
 
 
+states = ["Normal", "Safe", "DeTumble"]
+states.sort()
+
+def print_beacon(packet):
+    unpacked = struct.unpack('b'+'f'*11, packet[5:])
+    gyro = unpacked[3:6]
+    acc = unpacked[6:9]
+    mag = unpacked[9:12]
+    print(f"State:        {states[unpacked[0]]}")
+    print(f"CPU temp:     {unpacked[1]}")
+    print(f"IMU Temp:     {unpacked[2]}")
+    print(f"Gyro:         {gyro}")
+    print(f"Acceleration: {acc}")
+    print(f"Magnetometer: {mag}")
+
 def radio_test(rfm9x, LED):
     msg = ''
     last = None
@@ -16,7 +31,8 @@ def radio_test(rfm9x, LED):
             print("Received (raw header):", [hex(x) for x in packet[0:5]])
 
             if header == headers.DEFAULT:
-                print("unpacked: ", struct.unpack("f"*11, packet[5:]))
+                print_beacon(packet)
+                # print("unpacked: ", struct.unpack('b'+'f'*11, packet[5:]))
                 continue
 
             chunk = str(packet[5:], "ascii")
