ed1fa0f93fa403b9769eb82044e22d8810af58e8
Jacob Willis <jbwillis272@gmail.com>
2022-11-18 13:44:43 -0500

    increase wait between messages

--- radio_range_test/main.py
+++ radio_range_test/main.py
@@ -213,7 +213,7 @@ else:
             else:
                 rfm9x.send(bytes_msg)
                 print(f"Message {bold}{i+1}{normal}: Sent")
-            time.sleep(0.1*(i+1))
+            time.sleep(0.5*(i+1))
 
         repeat_str = get_input_discrete(
             f"Repeat transmission? {bold}(y/n){normal}", ["y", "n"])
